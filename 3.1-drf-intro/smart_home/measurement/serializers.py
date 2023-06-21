d3cc630272f49a437ebb59271fe74ab4c94c9275
Test <kia283@ya.ru>
2023-06-22 00:37:32 +1000

    Add 3.1-drf-intro

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,23 @@
 from rest_framework import serializers
-
+from .models import Sensor, Measurement
 # TODO: опишите необходимые сериализаторы
+
+
+# class SensorSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=256)
+#     description = serializers.CharField(max_length=256)
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Measurement
+        fields = ['sensor', 'temperature', 'created_at']
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,46 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer
+
+
+# @api_view(['GET', 'POST'])
+# def smart_home(request):
+#     if request.method == 'GET':
+#         sensor = Sensor.objects.all()
+#         data = SensorSerializer(sensor, many=True)
+#         return Response(data.data)
+#     if request.method == 'POST':
+#         return Response({'Status': 'Ok'})
+
+
+# class SmartHomeView(APIView):
+#     def get(self, request):
+#         sensor = Sensor.objects.all()
+#         data = SensorSerializer(sensor, many=True)
+#         return Response(data.data)
+#
+#     def post(self, request):
+#         return Response({'Status': 'Ok'})
+
+class SensorsView(ListCreateAPIView):
+    '''получение датчиков'''
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorView(RetrieveUpdateAPIView):
+    '''получение информации по датчику'''
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class MeasurementView(CreateAPIView):
+    """Добавить температуру"""
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
